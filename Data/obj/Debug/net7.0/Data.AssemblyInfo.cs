c177dc70d1cc89ed92f696a9bb2b28d5acb351b1
Admin <Admin@DESKTOP-DB0RLVI>
2025-05-17 21:30:01 +0700

    17/5

--- Data/obj/Debug/net7.0/Data.AssemblyInfo.cs
+++ Data/obj/Debug/net7.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad76d86b930baf4099d1394b2773922686078b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3b5de74a0dfd9ab22888c9381a2b011c55850e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ KLTN_YourLook/wwwroot/js_admin/Admin_Revenue.js
@@ -0,0 +1,52 @@
+﻿var chartInstance;
+
+function loadChart(year) {
+    $.ajax({
+        url: '/admin/getrevenuechart?year=' + year,
+        method: 'GET',
+        success: function (data) {
+            const ctx = document.getElementById('revenueChart').getContext('2d');
+
+            if (chartInstance) {
+                chartInstance.destroy(); // Xoá biểu đồ cũ nếu có
+            }
+
+            chartInstance = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: data.labels,
+                    datasets: [{
+                        label: 'Doanh thu năm '+ year,
+                        data: data.values,
+                        backgroundColor: 'rgba(54, 162, 235, 0.6)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            ticks: {
+                                callback: function (value) {
+                                    return value.toLocaleString('vi-VN');
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    });
+}
+
+// Khi trang load, gọi với năm hiện tại
+$(document).ready(function () {
+    const currentYear = $('#yearSelect').val();
+    loadChart(currentYear);
+
+    $('#yearSelect').on('change', function () {
+        const selectedYear = $(this).val();
+        loadChart(selectedYear);
+    });
+});
